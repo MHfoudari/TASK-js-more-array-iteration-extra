c1c312a9ea7db38369bbe940e38e000920cb13f3
MHfoudari <eng.mohammed.alfoudari@gmail.com>
2023-05-14 20:27:19 +0300

    finished the extra tasks

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const numbers = [1, 2, 3, 4, 5];
 // write code to create a new array where each
 // number is multiplied by 2 using the map method.
 // ============== Your answer here:
-
+let newNumber = numbers.map((rando) => 2 * rando);
+console.log(newNumber);
 // ==============
 
 // Q2)
@@ -13,7 +14,8 @@ const names = ["Alice", "Bob", "Charlie"];
 // write code to create a new array where each name is
 // converted to uppercase using the map method.
 // ============== Your answer here:
-
+let newNames = names.map((rando) => rando.toUpperCase());
+console.log(newNames);
 // ==============
 
 // Q3)
@@ -22,7 +24,8 @@ const numbers2 = [1, 4, 9, 16, 25];
 // write code to create a new array where each
 //  number is squared using the map method.
 // ============== Your answer here:
-
+let newNumber2 = numbers2.map((rando) => rando * rando);
+console.log(newNumber2);
 // ==============
 
 // Q4)
@@ -31,7 +34,8 @@ const names2 = ["John", "Jane", "Tom"];
 // write code to create a new array where each name
 // is appended with " Smith" using the map method.
 // ============== Your answer here:
-
+let newNames2 = names2.map((rando) => rando + " Smith");
+console.log(newNames2);
 // ==============
 
 // Q5)
@@ -40,7 +44,8 @@ const numbers3 = [2, 4, 6, 8, 10];
 //  write code to create a new array where each
 // number is divided by 2 using the map method.
 // ============== Your answer here:
-
+let newNumbers3 = numbers3.map((rando) => rando / 2);
+console.log(newNumbers3);
 // ==============
 
 // Q6)
@@ -49,5 +54,6 @@ const fruits = ["apple", "banana", "orange"];
 //  write code to create a new array where each fruit
 //  name is capitalized using the map method. Ex, Apple ...
 // ============== Your answer here:
-
+let newFruits = fruits.map((rando) => rando.charAt(0).toUpperCase() + rando.slice(1));
+console.log(newFruits);
 // ==============
